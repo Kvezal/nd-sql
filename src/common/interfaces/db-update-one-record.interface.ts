516af0301de333e86b78c323cb9b71b1070f55b8
Kvezal <muckam49@mail.ru>
2020-12-23 19:03:17 +0800

    interface description update

--- src/common/interfaces/db-update-one-record.interface.ts
+++ src/common/interfaces/db-update-one-record.interface.ts
@@ -1,4 +1,4 @@
 export interface IDbUpdateOneRecord {
-  updateOne<Type>(value?: Partial<Type>): Promise<Type[]>;
-  updateOne<Type>(value?: Partial<Type>, sql?: string): Promise<Type[]>;
+  updateOne<Type>(value?: Partial<Type>): Promise<Type>;
+  updateOne<Type>(value?: Partial<Type>, sql?: string): Promise<Type>;
 }
